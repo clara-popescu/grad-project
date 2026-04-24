98c4f2e9a6053e6ac7d0864a5230544f4c08d69c
clara <c.popescu0520231@arts.ac.uk>
2026-04-24 15:40:45 +0100

    change design to ipad

--- sketch.js
+++ sketch.js
@@ -1,99 +1,51 @@
-let stickyNote, textInput, panel, question;
-let submitBtn;
-let responses = [];
-let isTyping = false;
+let touchStartTime = 0;
+let held = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  select('body').style('user-select', 'none'); // disable text selection when moving the answer
-  select('body').style('background', '#111');
-
-  // Create a panel div and style it.
-  panel = createDiv('');
-  panel.size(400, 100);
-  panel.position(
-    (windowWidth - panel.width) / 2,
-    windowHeight - panel.height - 100
-  );
-  panel.style('border', '3px solid rgb(97,0,0)');
-  panel.style('border-radius', '20px');
-  panel.style('padding', '5px');
+  background(0, 0, 0);
+  stroke(255);
+  fill(255);
+  textSize(18);
   
-  // flex layout
-  panel.style('display', 'flex');
-  panel.style('flex-direction', 'column');
-  panel.style('align-items', 'center');
-  panel.style('gap', '10px'); // add spacing between elements
+  // 50px from edges
+  // |
+  let s = 'social class'
+  let w = textWidth(s);
+  text(s, (windowWidth/2)-(w/2), 30);
+  line(windowWidth/2, 50, windowWidth/2, windowHeight-50);
+  // arrow
+  line(windowWidth/2, 50, windowWidth/2-15, 50+25);
+  line(windowWidth/2, 50, windowWidth/2+15, 50+25);
   
-  // question and input
-  question = createDiv('how are you feeling today?')
-  panel.style('font-size', '30px');
-
-  textInput = createInput('');
-  textInput.size(200);
+  // -
+  line(50, windowHeight/2, windowWidth-50, windowHeight/2);
+  // arrow
+  triangle(50, windowHeight/2, 50+15, windowHeight/2-15, 50+15, windowHeight/2+15);
   
-  submitBtn = createButton('submit');
-  submitBtn.attribute('disabled', '');
+  // \
+  line(100, 100, windowWidth-100, windowHeight-100);
+  // /
+  line(windowWidth-100, 100, 100, windowHeight-100);
   
-  // Add input and btns to the panel.
-  question.parent(panel);
-  textInput.parent(panel);
-  submitBtn.parent(panel);
-
-  // Call functions when button is pressed and when text is input.
-  submitBtn.mousePressed(addStickyNote);
-  textInput.input(handleInput);
-}
-
-// Update stickyNote's HTML when text is input.
-function handleInput() {
-  // let value = textInput.value().trim();
-
-  // disable if empty, enable if not
-  if (textInput.value().trim().length === 0) {
-    submitBtn.attribute('disabled', '');
-  } else {
-    submitBtn.removeAttribute('disabled');
-  }
-  
-  // if the user started typing, create a new note
-  if (!isTyping && textInput.value().length > 0) {
-    newStickyNote();
-    isTyping = true;
-  }
-  
-  if (stickyNote) {
-    stickyNote.html(textInput.value());    
-  }
-}
-
-// Create a new div element and style it
-function newStickyNote() {
-  stickyNote = createDiv('...');
-  stickyNote.position(5, 5);
-  stickyNote.style('font-size', '40px');
-  stickyNote.style('background', 'rgb(224,224,224)');
-  stickyNote.style('padding', '5px 10px');
-  stickyNote.style('border-radius', '20px');
-  
-  // Make the note draggable.
-  stickyNote.draggable();
-}
-
-// Add each sticky note to the array so it stays on screen
-function addStickyNote() {
-  console.log('submitted');
-  responses.push(stickyNote);
-  
-  stickyNote.style('background', 'rgb(188, 185, 185)');
-  textInput.value('');
-  submitBtn.attribute('disabled', '');
-  isTyping = false;
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  if (held) {
+    let duration = millis() - touchStartTime;
+
+    if (duration >= 1000) {
+      circle(mouseX, mouseY, 30);
+      held = false; // prevent repeated triggering
+    }
+  }
+}
+
+function touchStarted() {
+  touchStartTime = millis();
+  held = true;
+}
+
+function touchEnded() {
+  held = false; // cancel if released early
 }
